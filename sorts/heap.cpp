37749406aa32f721abccf3ee558996ee5f254f66
Uros Aksentijevic <48104716+theaksaa@users.noreply.github.com>
2019-11-17 23:44:08 +0100

    Update heap.cpp

--- sorts/heap.cpp
+++ sorts/heap.cpp
@@ -1,56 +1,52 @@
 #include <stdio.h>
 #include <algorithm>
+using namespace std;
 
 void heapadd(int *x, int i)
 {
-    while ( i && x[i] < x[ (i - 1) / 2 ] )
+    while (i && x[i] < x[(i - 1) / 2])
     {
-        std::swap( x[i], x[ (i - 1 ) / 2 ] );
-        i = ( i - 1 ) / 2;
+        swap(x[i], x[(i - 1) / 2]);
+        i = (i - 1) / 2;
     }
 }
 
 void heap(int *x, int d )
 {
-    int k = x[ 0 ], i = 0;
-    x[ 0 ] = x[ d ];
+    int k = x[0], i = 0;
+    x[0] = x[d];
 
-    while ( 2 * i + 2 < d )
-        if ( x[ 2 * i + 1 ] > x[ 2 * i + 2 ] )
+    while (2 * i + 2 < d)
+        if (x[2 * i + 1] > x[2 * i + 2])
         {
-            if ( x[ 2 * i + 2 ] > x[ i ] ) std::swap( k,  x[ 2 * i + 2 ] );
-            std::swap( x[ i ], x[ 2 * i + 2 ] );
+            if (x[2 * i + 2] > x[i]) swap(k,  x[2 * i + 2]);
+            swap(x[i], x[2 * i + 2]);
             i = 2 * i + 2;
         }
         else
         {
-            if ( x[ 2 * i + 1 ] > x[ i ] ) std::swap( k, x[ 2 * i + 1 ] );
-            std::swap( x[ i ], x[ 2 * i + 1 ] );
+            if (x[2 * i + 1] > x[i]) swap(k, x[2 * i + 1]);
+            swap(x[i], x[2 * i + 1]);
             i = 2 * i + 1;
         }
 
-    if ( d - 1 == 2 * i + 1 && x[ 2 * i + 1 ] < x[ i ] ) std::swap( x[ i ], x[ 2 * i + 1 ] );
+    if (d - 1 == 2 * i + 1 && x[2 * i + 1] < x[i]) swap(x[i], x[2 * i + 1]);
 }
 
 void heapsort(int *x, int n)
 {
-    for ( int i = 0; i < n; i++ ) heapadd( x, i );
-    
-    for ( int i = n - 1; i >= 0; i -- ) heap( x, i );
+    for (int i = 0; i < n; i++) heapadd(x, i);
+    for (int i = n - 1; i >= 0; i--) heap(x, i);
 }
 
 int main()
 {
-    FILE *fin = fopen("in.in", "r");
-    FILE *fout = fopen("out.out", "w");
-        
     int n;
-    fscanf(fin, "%d", &n);
+    scanf("%d", &n);
     int x[n];
 
-    for ( int i = 0; i < n; i++ ) fscanf(fin, "%d", &x[i]);
-    heapsort( x, n );
+    for (int i = 0; i < n; i++) scanf("%d", &x[i]);
+    heapsort(x, n);
     
-    for ( int i = 0; i < n; i++ ) printf("%d ", x[i]);
-    printf("\n");
+    for (int i = 0; i < n; i++) printf("%d ", x[i]);
 }
